ef62cdd3cca4e480e94c80b6f7e788fb598daba6
vijetha <lvijetha90@gmail.com>
2016-12-11 12:07:11 +0530

    design changes

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,5 +1,32 @@
-<?php include('header.php'); ?>
- <div>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Admin Panel</title>
+
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url('vendors/css/bootstrap.css');?>" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="<?php echo base_url('vendors/css/font-awesome.min.css');?>"  rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?php echo base_url('vendors/css/nprogress.css');?>" rel="stylesheet">
+    <!-- bootstrap-progressbar -->
+    <link href="<?php echo base_url('vendors/css/bootstrap-progressbar-3.3.4.min.css');?>" rel="stylesheet">
+	
+    <!-- bootstrap-daterangepicker -->
+    <link href="<?php echo base_url('vendors/css/daterangepicker.css');?>"  rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="<?php echo base_url('vendors/css/custom.min.css');?>" rel="stylesheet">
+
+	 <link href="<?php echo base_url('vendors/css/bootstrap-table.css ');?>" rel="stylesheet">
+ </head>
+	 <div>
       <a class="hiddenanchor" id="signup"></a>
       <a class="hiddenanchor" id="signin"></a>
 
@@ -35,8 +62,8 @@
                 <br />
 
                 <div>
-                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
-                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                  <h1><i class="fa fa-paw"></i> Admin panel!</h1>
+                  <p>©2016 All Rights Reserved. Privacy and Terms</p>
                 </div>
               </div>
             <?php echo  form_close(); ?>
@@ -46,3 +73,4 @@
         
       </div>
     </div>
+</html>
