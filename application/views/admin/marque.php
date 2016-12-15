e7626ae21ddf20c8bdba336271eb1c17ec9244d7
vijetha <lvijetha90@gmail.com>
2016-12-15 22:05:28 +0530

    changes

---
+++ application/views/admin/marque.php
@@ -0,0 +1,11 @@
+<?php if($this->auth->check_access('Restaurant manager')) : ?>
+<div style="margin-top:40px;border:1px solid red;text-align:center;overflow-y:scroll;">
+<?php $messages = $this->Restaurant_model->GetMessages(); ?>
+<MARQUEE WIDTH=100% HEIGHT=100 >
+<?php if(isset($messages['data'])){
+	foreach($messages['data'] as $message){ ?> 
+	<div style="margin-right:20px;color:red;"><strong><?=$message['restaurant_name'];?>:</strong> <?=$message['message']; ?></div>
+<?php } } ?>
+</MARQUEE>
+</div>
+<?php endif; ?>
