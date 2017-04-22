27bf9f3dacb74fcae3c325af477a07bd05c04707
bigperlnagaraj <nagaraj.bigperl@gmail.com>
2017-04-22 13:12:55 +0530

    restaurant valid

--- application/views/admin/restaurant_form.php
+++ application/views/admin/restaurant_form.php
@@ -48,16 +48,13 @@
 					<input type="number" name="restaurant_mobile" value="<?=$restaurant_mobile?>" class="form-control"  pattern="/(7|8|9)\d{9}/" required>
 					</div>
 				</div>
-				
 				<div class="form-group">	
 					<label for="restaurant_address">Restaurant Manager Mobile No</label>
-					<div class="input-group col-lg-12">
+					<div class="input-group">
 						
-						
-					<input type="text" name="restaurant_mngrmobile" value="<?=$restaurant_mobile?>" class="form-control"  pattern="/(7|8|9)\d{9}/" required>
+						<input type="number" name="restaurant_mngrmobile" value="<?=$restaurant_mobile?>" class="form-control"  pattern="/(7|8|9)\d{9}/" required>
 					</div>
 				</div>
-				
 				<div class="form-group">
 					<label for="restaurant_address">Restaurant email</label>
 					<?php
