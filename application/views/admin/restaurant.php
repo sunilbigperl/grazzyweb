4713e01871b954badb6283b6be601f705656b1f3
bigpelganga <gkamatagi@gmail.com>
2017-05-25 18:14:40 +0530

    change

--- application/views/admin/restaurant.php
+++ application/views/admin/restaurant.php
@@ -57,12 +57,15 @@
 				
 									<?php } ?>
 					<a class="btn btn-xs btn-primary" href="<?php echo site_url($this->config->item('admin_folder').'/menus/index/'.$restaurant->restaurant_id); ?>">Menu</a></br>
+				<?php if($this->auth->check_access('Admin')) {?>
 					<?php if($restaurant->enabled == 1){ ?> 
 						<a class="btn btn-xs btn-danger" style="color: white;" data-toggle="modal" data-target="#DeactivateRest" onclick="$('#restid').val('<?=$restaurant->restaurant_id;?>')">Deactivate</a></br>
+
 					<?php }else{ ?>
 						
 						<a class="btn btn-xs btn-success" style="color:white;" href="<?php echo site_url($this->config->item('admin_folder').'/restaurant/RestaurantStatusChange/'.$restaurant->restaurant_id."/1"); ?>" >Activate</a>
 					<?php } ?>
+                     <?php } ?>
 					</span>
 				</td>
 				<td>
@@ -149,5 +152,23 @@
 			}
 		});
 	}
-</script>  
+</script> 
+<script type="text/javascript">
+    var datefield=document.createElement("input")
+    datefield.setAttribute("type", "date")
+    if (datefield.type!="date"){ //if browser doesn't support input type="date", load files for jQuery UI Date Picker
+        document.write('<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />\n')
+        document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"><\/script>\n')
+        document.write('<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"><\/script>\n') 
+    }
+</script>
+ 
+<script>
+if (datefield.type!="date"){ //if browser doesn't support input type="date", initialize date picker widget:
+    jQuery(function($){ //on document.ready
+        $('#FromDate').datepicker();
+        $('#ToDate').datepicker();
+    })
+}
+</script> 
 <?php $this->load->view('admin/marque'); ?>
