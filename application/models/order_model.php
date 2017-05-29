d31d93a73257b4c0342c9e0605b1e0d714899558
bigpelganga <gkamatagi@gmail.com>
2017-05-29 12:31:23 +0530

    changed status

--- application/models/order_model.php
+++ application/models/order_model.php
@@ -54,7 +54,7 @@ Class order_model extends CI_Model
 		// echo "SELECT a.*,d.order_type,d.ordertype_id,b.* FROM `orders` a, restaurant b, order_type d, admin c WHERE  a.`restaurant_id` = b.restaurant_id 
 		// and d.ordertype_id =a.order_type and b.restaurant_manager = c.id and b.restaurant_manager='".$userdata['id']."' and a.ordered_on >='".$date."' order by a.ordered_on desc"; exit;
 		$sql = $this->db->query("SELECT a.*,d.order_type,d.ordertype_id,b.* FROM `orders` a, restaurant b, order_type d, admin c WHERE  a.`restaurant_id` = b.restaurant_id 
-		and d.ordertype_id =a.order_type and b.restaurant_manager = c.id and b.restaurant_manager='".$userdata['id']."' and a.ordered_on >='".$date."' order by a.ordered_on desc");
+		and d.ordertype_id =a.order_type and b.restaurant_manager = c.id and b.restaurant_manager='".$userdata['id']."' and status='Order Placed' and a.ordered_on >='".$date."' order by a.ordered_on desc");
 		if($sql->num_rows() > 0){
 			$result	= $sql->result();
 		}else{
