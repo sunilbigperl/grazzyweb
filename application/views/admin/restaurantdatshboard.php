01aeefca52ac3e9b3e5439d4c23a3b81d1e6ee6a
bigpelganga <gkamatagi@gmail.com>
2017-06-06 18:20:20 +0530

    Delivery time and Keep ready by order time issue in POS Panel

--- application/views/admin/restaurantdatshboard.php
+++ application/views/admin/restaurantdatshboard.php
@@ -71,7 +71,7 @@ echo "" . date("jS F Y") . "<br>";
       
                  
 ?>
-				<td><?php echo date("h:i:s", strtotime("$t1-$order->preparation_time minutes"));?></td>
+				<td><?php echo date("h:i:s", strtotime("$t1-$order->preparation_time minutes"));?></td> 
 				 
 				
 				<!--<td> 
