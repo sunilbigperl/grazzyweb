bff4c1f4867ffa9f34fadfe8adc1f0ecf623bdba
bigpelganga <gkamatagi@gmail.com>
2017-07-11 14:37:38 +0530

    change

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -133,7 +133,7 @@ class Api_model extends CI_Model
 			$where ='';
 			
 			if(isset($data['area']) && $data['area'] != ""){
-				$sql ="SELECT *,( 3959 * acos( cos( radians('".$data['latitude']."') ) * cos( radians( restaurant_latitude ) ) * cos( radians( restaurant_langitude ) - radians('".$data['langitude']."') ) + sin( radians('".$data['latitude']."') ) * sin( radians( restaurant_latitude ) ) ) ) AS distance FROM restaurant   HAVING distance < 4 and restaurant_branch like '%".$data['area']."%'
+				$sql ="SELECT *,( 3959 * acos( cos( radians('".$data['latitude']."') ) * cos( radians( restaurant_latitude ) ) * cos( radians( restaurant_langitude ) - radians('".$data['langitude']."') ) + sin( radians('".$data['latitude']."') ) * sin( radians( restaurant_latitude ) ) ) ) AS distance FROM restaurant   HAVING distance < 4 and restaurant_address like '%".$data['area']."%'
 				and enabled=1 and `delete`=0";
 			}
 			if(isset($data['name']) && $data['name'] != ""){
