ff22c67e004d9fbb9095198f42ed062bb3b67958
bigperlganaga <gkamatagi@gmail.com>
2017-07-13 11:58:31 +0530

    EATSAPP-64 Delivery is still hard coded @ 40..... It should be coming from Admin Panel-Fixed

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -538,7 +538,8 @@ class Api_model extends CI_Model
 		}
 	}
 	public function getMenus($id){
-		$sql3 = $this->db->query("select servicetax,delivery_charge from restaurant where restaurant_id='".$id."'");
+		// $sql3 = $this->db->query("select servicetax,delivery_charge from restaurant where restaurant_id='".$id."'");
+		$sql3 = $this->db->query("select servicetax,deliverycharge from charges where id='".$id."'");
 		$servicetax =  $sql3->result_array();
 		
 		$sql ="SELECT DISTINCT b.category_id,  c.parent_id, c.name FROM `restaurant_menu` a, menu_categories b, categories c where 
@@ -554,7 +555,7 @@ class Api_model extends CI_Model
 					$result[$i]['category_id'] = $menu['category_id'];
 					$result[$i]['category'] = $menu['name'];
 					$result[$i]['servicetax'] =  $servicetax[0]['servicetax'];
-					$result[$i]['delivery_charge'] =  $servicetax[0]['delivery_charge'];
+					$result[$i]['deliverycharge'] =  $servicetax[0]['deliverycharge'];
 					$sql1 ="SELECT * FROM `restaurant_menu` a, menu_categories b, categories c where a.restaurant_id = '".$id."' and b.category_id='".$menu['category_id']."' 
 					and a.menu_id = b.menu_category and b.category_id = c.id and a.`delete`=0 and a.`enabled`=1";
 					//echo $sql1; exit;
