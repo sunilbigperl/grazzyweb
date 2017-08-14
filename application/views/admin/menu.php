be495bcd90b085954eb12029eee1d090966830d5
bigpelganga <gkamatagi@gmail.com>
2017-08-14 12:48:33 +0530

    change the column heading

--- application/views/admin/menu.php
+++ application/views/admin/menu.php
@@ -20,7 +20,7 @@ var oTable = $('#table-pagination').DataTable( {
 		   text: 'Export menu',
            footer: false,
 		   exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4,5,6,7,8]
+                    columns: [ 0, 1, 2, 3, 4,5,6,7,8,9]
             },
        }        
     ]  
