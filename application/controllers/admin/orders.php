4023cc0746aaf5c0210cc69cf1a559a8e0598743
bigpelganga <gkamatagi@gmail.com>
2017-10-16 12:26:23 +0530

    change

--- application/controllers/admin/orders.php
+++ application/controllers/admin/orders.php
@@ -208,7 +208,7 @@ class Orders extends Admin_Controller {
 		if($this->input->post('action') == "Go"){
 			$data['fromdate'] =  $_SESSION['fromdate'] = date("Y-m-d",strtotime($this->input->post('fromdate')));
 			// $data['todate'] = $_SESSION['todate'] = date("Y-m-d",strtotime($this->input->post('todate')));
-			$data['todate'] = $_SESSION['todate'] = date('Y-m-d H:i:s',strtotime($this->input->post('todate').' +1 day'));
+			$data['todate'] = $_SESSION['todate'] = date('Y-m-d',strtotime($this->input->post('todate').' +1 day'));
 		}elseif($this->input->post('action') == "PreviousMonth"){
 			$data['fromdate'] =  $_SESSION['fromdate'] =  date('Y-m-d',strtotime('first day of last month'));
 			$data['todate'] = $_SESSION['todate'] = date('Y-m-d',strtotime('last day of last month'));
