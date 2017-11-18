7996bb8442721a38db28e93d276f702ef1e46f56
bigpelganga <gkamatagi@gmail.com>
2017-11-18 12:14:30 +0530

    EATSAPP-287 Remove the Amount column from popup box and also show the cost without convenience charges -Done

--- application/models/order_model.php
+++ application/models/order_model.php
@@ -53,7 +53,7 @@ Class order_model extends CI_Model
 		$date = date("Y-m-d 00:00:00");
 		// echo "SELECT a.*,d.order_type,d.ordertype_id,b.* FROM `orders` a, restaurant b, order_type d, admin c WHERE  a.`restaurant_id` = b.restaurant_id 
 		// and d.ordertype_id =a.order_type and b.restaurant_manager = c.id and b.restaurant_manager='".$userdata['id']."' and a.ordered_on >='".$date."' order by a.ordered_on desc"; exit;
-		$sql = $this->db->query("SELECT a.*,d.order_type,d.ordertype_id,b.* FROM `orders` a, restaurant b, order_type d, admin c WHERE  a.`restaurant_id` = b.restaurant_id 
+		$sql = $this->db->query("SELECT a.*,d.order_type,d.ordertype_id,b.restaurant_name,b.preparation_time FROM `orders` a, restaurant b, order_type d, admin c WHERE  a.`restaurant_id` = b.restaurant_id 
 		and d.ordertype_id =a.order_type and b.restaurant_manager = c.id and b.restaurant_manager='".$userdata['id']."' and a.status IN ('Order Placed', 'Assigned','Accepted','Picked Up')  and a.ordered_on >='".$date."' order by a.ordered_on desc");
 		if($sql->num_rows() > 0){
 			$result	= $sql->result();
--- application/views/admin/restaurantdatshboard.php
+++ application/views/admin/restaurantdatshboard.php
@@ -53,7 +53,10 @@ echo "" . date("jS F Y") . "<br>";
 					<a href="#" style="color: #2f2fd0;text-decoration:underline;" data-toggle="modal" data-target="#orderdetails" onclick="showdetails('<?php echo site_url($this->config->item('admin_folder').'/orders/getMenuDetails');?>',<?=htmlspecialchars(json_encode($order));?>);"><?=$order->order_number;?></a>
 				</td>
 				<td>
-					<?=$order->total_amount; ?>
+				     <?php $cost=$order->total_amount-$order->discount1-$order->discount2+$order->tax;?>
+				     
+					<!-- <?=$order->total_amount; ?> -->
+					<?=$cost; ?>
 				</td>
 				<td>
 					<?=$order->ordered_on; ?>
