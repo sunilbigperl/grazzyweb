7411771f8f2c2495627c51140e73a4173122b914
bigpelganga <gkamatagi@gmail.com>
2018-04-02 17:34:57 +0530

    change

--- application/controllers/admin/orders.php
+++ application/controllers/admin/orders.php
@@ -359,7 +359,7 @@ class Orders extends Admin_Controller {
         $this->m_pdf->pdf->WriteHTML($html);
 		$this->m_pdf->pdf->Output($filename, "F");
 	    //redirect("http://localhost/grazzyweb/".$filename);
-		redirect("https://app.eatsapp.in/".$filename);
+		redirect("http://eatsapp.in/login/".$filename);
 
 	}
 
@@ -1384,7 +1384,7 @@ $export_excel = $this->db->query("SELECT a.*,d.order_type,d.ordertype_id,b.resta
         $this->m_pdf->pdf->WriteHTML($html);
 		$this->m_pdf->pdf->Output($filename, "F");
 		
-		redirect("https://app.eatsapp.in/".$filename);
+		redirect("http://eatsapp.in/login/".$filename);
 		//redirect("http://localhost/grazzyweb/".$filename);
 	}
 	
