4b34e018cdc0f1e45386bfb18d94499262862df1
bigpelganga <gkamatagi@gmail.com>
2018-05-31 13:05:46 +0530

    change

--- application/models/order_model.php
+++ application/models/order_model.php
@@ -212,7 +212,7 @@ Class order_model extends CI_Model
 			// and d.ordertype_id =a.order_type and b.restaurant_manager = c.id   and (a.ordered_on >= '".$data['fromdate']."' and a.ordered_on <= '".$data['todate']."') ".$where."
 			//  order by ordered_on desc"; exit; 
 			$sql = $this->db->query("SELECT a.*,d.order_type,d.ordertype_id,b.restaurant_name,e.firstname,e.phone FROM `orders` a, restaurant b, order_type d, admin c,customers e WHERE  a.`restaurant_id` = b.restaurant_id and a.`customer_id` = e.id and a.status!='Payment pending'
-			and d.ordertype_id =a.order_type and b.restaurant_manager = c.id   and (a.ordered_on >= '".$data['fromdate']."' and a.ordered_on < '".$data['todate']."') ".$where."
+			and d.ordertype_id =a.order_type and b.restaurant_manager = c.id   and (a.ordered_on >= '".$data['fromdate']."' and a.ordered_on <= '".$data['todate']."') ".$where."
 			 order by ordered_on desc");
 			
 			
