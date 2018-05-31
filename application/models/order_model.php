fb03b6ee7d683d056b3f2b3fad4b54f1676416ca
bigpelganga <gkamatagi@gmail.com>
2018-05-31 12:48:34 +0530

    change

--- application/models/order_model.php
+++ application/models/order_model.php
@@ -184,7 +184,7 @@ Class order_model extends CI_Model
 		if($this->auth->check_access('Restaurant manager')){
 		
 			$sql = $this->db->query("SELECT a.*,d.order_type,d.ordertype_id,b.restaurant_name FROM `orders` a, restaurant b, order_type d, admin c,customers e WHERE  a.`restaurant_id` = b.restaurant_id and a.`customer_id` = e.id  
-		and d.ordertype_id =a.order_type and b.restaurant_manager = c.id and b.restaurant_manager='".$userdata['id']."' and a.ordered_on >= '".$data['fromdate']."' and a.ordered_on < '".$data['todate']."'  and a.status IN ('Delivered', 'Shipped','Rejected','order cancelled')  order by ordered_on desc");
+		and d.ordertype_id =a.order_type and b.restaurant_manager = c.id and b.restaurant_manager='".$userdata['id']."' and a.ordered_on >= '".$data['fromdate']."' and a.ordered_on <= '".$data['todate']."'  and a.status IN ('Delivered', 'Shipped','Rejected','order cancelled')  order by ordered_on desc");
 		}elseif($this->auth->check_access('Deliver manager')){
 			
 			if($this->auth->check_access('Deliver manager')){
@@ -278,7 +278,7 @@ Class order_model extends CI_Model
 		// and d.ordertype_id =a.order_type and b.restaurant_manager = c.id and b.restaurant_id='".$data['id']."' and a.ordered_on >= '".$data['fromdate']."' and a.ordered_on <= '".$data['todate']."' order by ordered_on desc"; exit;
 
 		$sql = $this->db->query("SELECT a.*,d.order_type,d.ordertype_id,b.restaurant_name,e.firstname,e.phone FROM `orders` a, restaurant b, order_type d, admin c,customers e WHERE  a.`restaurant_id` = b.restaurant_id and a.`customer_id` = e.id
-		and d.ordertype_id =a.order_type and b.restaurant_manager = c.id and b.restaurant_id='".$data['id']."' and a.ordered_on >= '".$data['fromdate']."' and a.ordered_on <'".$data['todate']."' order by ordered_on desc");
+		and d.ordertype_id =a.order_type and b.restaurant_manager = c.id and b.restaurant_id='".$data['id']."' and a.ordered_on >= '".$data['fromdate']."' and a.ordered_on <='".$data['todate']."' order by ordered_on desc");
 		
 		if($sql->num_rows() > 0){
 			$result	= $sql->result();
