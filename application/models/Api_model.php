063614637860adc37f782389a28be13553f5e501
ganga <gkamatagi@gmail.com>
2018-06-15 17:19:35 -0700

    change

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -180,7 +180,7 @@ class Api_model extends CI_Model
 	
 	public function delboyOrders($id){
 		//echo "select * from orders a , customers b, restaurant c where a.customer_id=b.id and a.restaurant_id = c.restaurant_id and a.delivered_by != 0 and a.delivered_by='".$id."' and a.order_type != 3"; exit;
-		$threadmsg = $this->db->query("select a.*, b.firstname,b.lastname, b.email, b.phone, c.* from orders a , customers b, restaurant c where a.customer_id=b.id and a.restaurant_id = c.restaurant_id and a.delivered_by != 0  and a.delivered_by='".$id."' and  a.status!='order cancelled' and a.order_type != 3  order by a.id desc");
+		$threadmsg = $this->db->query("select a.*, b.firstname,b.lastname, b.email, b.phone, c.* from orders a , customers b, restaurant c where a.customer_id=b.id and a.restaurant_id = c.restaurant_id and a.delivered_by != 0  and a.delivered_by='".$id."' and  a.status!='order cancelled' and a.order_type != 3  order by a.ordered_on desc");
 
 
 
