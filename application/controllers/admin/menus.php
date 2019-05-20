2690c2e9a522812f80fcd79ba696fac3916909b8
ganga <gkamatagi@gmail.com>
2019-05-20 14:25:50 +0530

    change

--- application/controllers/admin/menus.php
+++ application/controllers/admin/menus.php
@@ -100,6 +100,7 @@ class Menus extends Admin_Controller {
 		
         $this->form_validation->set_rules('menu', 'lang:menu', 'trim|required');
 		$this->form_validation->set_rules('type', 'lang:type', 'trim|required');
+		$this->form_validation->set_rules('type', 'lang:itemPreparation_time', 'trim|required');
         $this->form_validation->set_rules('price', 'lang:price', 'trim');
 		$this->form_validation->set_rules('image', 'lang:image', 'trim');
 		 
