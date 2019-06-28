0bc1c3d8247cea70e49a68667c7525e11021db8d
bigperlsunil <sunil@bigperl.com>
2019-06-28 15:48:00 +0530

    redirecting url from http to https in jio_test files

--- jio_test/jio_curl.php
+++ jio_test/jio_curl.php
@@ -1,61 +1,121 @@
-<?php
-
-echo "1";
-
-$url = 'https://testpg.rpay.co.in/reliance-webpay/v1.0/jiopayments';
-$postData = array();
-$postData["merchantid"] = "100001000014146";
-$postData["clientid"] = "10000002";
-$postData["channel"] = "WEB";
-$postData["returl"] = "http://eatsapp.in/login/jio_test/response.php";
-$postData["checksum"] = "5cf6736717ebce51966949dc25ea2c088706515548fe9b8b8f6ee14d3496f23c ";
-$postData["token"] = "";
-$postData["transaction.extref"] = "123123123";
-$postData["transaction.timestamp"] = "20170405125217";
-$postData["transaction.txntype"] = "PURCHASE";
-$postData["transaction.amount"] = "1.00";
-$postData["transaction.currency"] = "INR";
-$postData["transaction.mobilenumber"] = "820569065";
-
-echo "2";
-
-/*
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-
-echo "3";
-
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo "4";
-echo $result;
-
-print_r ("result is".$result);
-*/
-
-
-$query = http_build_query ($postData);
-
-$contextData = array ( 
-                'method' => 'POST',
-                'header' => "Connection: close\r\n".
-                            "Content-Length: ".strlen($query)."\r\n",
-                'content'=> $query );
- 
-// Create context resource for our request
-$context = stream_context_create (array ( 'http' => $contextData ));
- 
-// Read page rendered as result of your POST request
-$result =  file_get_contents (
-                  'https://testpg.rpay.co.in/reliance-webpay/v1.0/jiopayments',  // page url
-                  false,
-                  $context);
-
-echo $result;
-print_r ("result is".$result);				  
-				  
+<?php
+
+
+
+echo "1";
+
+
+
+$url = 'https://testpg.rpay.co.in/reliance-webpay/v1.0/jiopayments';
+
+$postData = array();
+
+$postData["merchantid"] = "100001000014146";
+
+$postData["clientid"] = "10000002";
+
+$postData["channel"] = "WEB";
+
+$postData["returl"] = "https://eatsapp.in/login/jio_test/response.php";
+
+$postData["checksum"] = "5cf6736717ebce51966949dc25ea2c088706515548fe9b8b8f6ee14d3496f23c ";
+
+$postData["token"] = "";
+
+$postData["transaction.extref"] = "123123123";
+
+$postData["transaction.timestamp"] = "20170405125217";
+
+$postData["transaction.txntype"] = "PURCHASE";
+
+$postData["transaction.amount"] = "1.00";
+
+$postData["transaction.currency"] = "INR";
+
+$postData["transaction.mobilenumber"] = "820569065";
+
+
+
+echo "2";
+
+
+
+/*
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+
+curl_setopt($ch, CURLOPT_POST, 1);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+
+
+
+echo "3";
+
+
+
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+
+
+echo "4";
+
+echo $result;
+
+
+
+print_r ("result is".$result);
+
+*/
+
+
+
+
+
+$query = http_build_query ($postData);
+
+
+
+$contextData = array ( 
+
+                'method' => 'POST',
+
+                'header' => "Connection: close\r\n".
+
+                            "Content-Length: ".strlen($query)."\r\n",
+
+                'content'=> $query );
+
+ 
+
+// Create context resource for our request
+
+$context = stream_context_create (array ( 'http' => $contextData ));
+
+ 
+
+// Read page rendered as result of your POST request
+
+$result =  file_get_contents (
+
+                  'https://testpg.rpay.co.in/reliance-webpay/v1.0/jiopayments',  // page url
+
+                  false,
+
+                  $context);
+
+
+
+echo $result;
+
+print_r ("result is".$result);				  
+
+				  
+
 ?> 
--- jio_test/purchase.php
+++ jio_test/purchase.php
@@ -1,78 +1,155 @@
-<?php
- date_default_timezone_set('Asia/Calcutta');
-
-	
-$order_id =$_GET["order_id"];//rand(1,10000000);//"9KZJLV8VDUGK";
-$amount =$_GET["amount"];//rand(1,10000000);//"9KZJLV8VDUGK";
-	
-
-$timestamp = date('YmdHis');// "20170405125217";
-
-	
-$checksumSeed = "tSLfi8BMxohvWJTCfwd1cZuARH78myF21JAdgdNhvixbj7o6+uIA38WFm7VHQ0aGu8LyQYv8tRPyN+Ba0+nRLuBLZXK4PH2gxkSvJ7Jnhof0NJr3IktRPSUZIQi6hcFZxoElSuOD8KzxCkagJfxT/u4vLeGqdskKl3p46RxJAJvOp7VutGHK2MG1HE7X68E/cKJrEUk7v0vI+kUp2Mfyh7GE8wZ9enYBrkY6olGYS9pLHLv/zGAZzAZVadblK1+3";
-
-//Clientid|Amount|Extref|Channel|MerchantId|Token|Return Url|Txn TimeStamp|TxnType
-	
-$data = "10000002|".$amount."|".$order_id."|WEB|100001000014146||http://eatsapp.in/login/jio_test/response.php|".$timestamp."|PURCHASE";
-
-	
-$checksum = hash_hmac('SHA256',$data, $checksumSeed);
-//$checksum = "070d0d1145dd0a0f6524f8f537b7ebd4f1c6008fe7f25694acd6bd85b5a929d5";
-
-	//001000003096
-	
-	//echo $checksum;
-	
-	$url = "http://eatsapp.in/login/jio_test/response.php";
-?>
-
-
-<html>
-<head>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-<script type="text/javascript">
-function nithin()
-{
-console.log("a");
- $( "#form1" ).submit();
-console.log("b");
-
-}
-
-   
-</script>
-
-</head>
-
-<body onload="nithin()" style="background:#ededee"> 
-
-<img src="eatsapp_loading.gif" style="display: block;margin: auto;">
-<p style="display: none;text-align: center;font-weight: bold;font-family: arial;">Eatsapp</p>
-<p style="display: none;text-align: center;/*! font-weight: bold; */font-family: arial;">All the transactions made through Eatsapp are secure.Please click on proceed to pay to continue.</p>
-
-<form name="payment" id="form1" method="POST" action="https://testpg.rpay.co.in/reliance-webpay/v1.0/jiopayments">
-<input type='hidden' name="merchantid" value="100001000014146"/>	
-<input type='hidden' name="clientid" value="10000002"/>	
-<input type='hidden' name="channel" value="WEB"/>	
-<input type='hidden' name="returl" value="<?php echo $url; ?>"/>	
-<input type="hidden" name="checksum" value="<?php echo $checksum; ?>"/>	 
-<input type='hidden' name="token" value=""/>	
-<!-- Transaction Info -->	 
-<input type="hidden" name="transaction.extref" value="<?php echo $order_id; ?>"/>	
-<input type="hidden" name="transaction.timestamp" value="<?php echo $timestamp; ?>"/>	
-<input type='hidden' name="transaction.txntype" value="PURCHASE"/>	
-<input type='hidden' name="transaction.amount" value="<?php echo $amount; ?>"/>
-<input type="hidden" name="transaction.currency" value="INR"/>
-<input type="hidden" name="subscriber.mobilenumber" value="820569065"/>
-
-
-</form>
-
-<input onclick="nithin()" name="submit" style="display: none;margin: auto;background: #52c600;color: white;font-weight: bold;padding: 10px;border: none;" value="Proceed to Pay" type="button">
-
-</body>
-
-
-
-
+<?php
+
+ date_default_timezone_set('Asia/Calcutta');
+
+
+
+	
+
+$order_id =$_GET["order_id"];//rand(1,10000000);//"9KZJLV8VDUGK";
+
+$amount =$_GET["amount"];//rand(1,10000000);//"9KZJLV8VDUGK";
+
+	
+
+
+
+$timestamp = date('YmdHis');// "20170405125217";
+
+
+
+	
+
+$checksumSeed = "tSLfi8BMxohvWJTCfwd1cZuARH78myF21JAdgdNhvixbj7o6+uIA38WFm7VHQ0aGu8LyQYv8tRPyN+Ba0+nRLuBLZXK4PH2gxkSvJ7Jnhof0NJr3IktRPSUZIQi6hcFZxoElSuOD8KzxCkagJfxT/u4vLeGqdskKl3p46RxJAJvOp7VutGHK2MG1HE7X68E/cKJrEUk7v0vI+kUp2Mfyh7GE8wZ9enYBrkY6olGYS9pLHLv/zGAZzAZVadblK1+3";
+
+
+
+//Clientid|Amount|Extref|Channel|MerchantId|Token|Return Url|Txn TimeStamp|TxnType
+
+	
+
+$data = "10000002|".$amount."|".$order_id."|WEB|100001000014146||https://eatsapp.in/login/jio_test/response.php|".$timestamp."|PURCHASE";
+
+
+
+	
+
+$checksum = hash_hmac('SHA256',$data, $checksumSeed);
+
+//$checksum = "070d0d1145dd0a0f6524f8f537b7ebd4f1c6008fe7f25694acd6bd85b5a929d5";
+
+
+
+	//001000003096
+
+	
+
+	//echo $checksum;
+
+	
+
+	$url = "https://eatsapp.in/login/jio_test/response.php";
+
+?>
+
+
+
+
+
+<html>
+
+<head>
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+<script type="text/javascript">
+
+function nithin()
+
+{
+
+console.log("a");
+
+ $( "#form1" ).submit();
+
+console.log("b");
+
+
+
+}
+
+
+
+   
+
+</script>
+
+
+
+</head>
+
+
+
+<body onload="nithin()" style="background:#ededee"> 
+
+
+
+<img src="eatsapp_loading.gif" style="display: block;margin: auto;">
+
+<p style="display: none;text-align: center;font-weight: bold;font-family: arial;">Eatsapp</p>
+
+<p style="display: none;text-align: center;/*! font-weight: bold; */font-family: arial;">All the transactions made through Eatsapp are secure.Please click on proceed to pay to continue.</p>
+
+
+
+<form name="payment" id="form1" method="POST" action="https://testpg.rpay.co.in/reliance-webpay/v1.0/jiopayments">
+
+<input type='hidden' name="merchantid" value="100001000014146"/>	
+
+<input type='hidden' name="clientid" value="10000002"/>	
+
+<input type='hidden' name="channel" value="WEB"/>	
+
+<input type='hidden' name="returl" value="<?php echo $url; ?>"/>	
+
+<input type="hidden" name="checksum" value="<?php echo $checksum; ?>"/>	 
+
+<input type='hidden' name="token" value=""/>	
+
+<!-- Transaction Info -->	 
+
+<input type="hidden" name="transaction.extref" value="<?php echo $order_id; ?>"/>	
+
+<input type="hidden" name="transaction.timestamp" value="<?php echo $timestamp; ?>"/>	
+
+<input type='hidden' name="transaction.txntype" value="PURCHASE"/>	
+
+<input type='hidden' name="transaction.amount" value="<?php echo $amount; ?>"/>
+
+<input type="hidden" name="transaction.currency" value="INR"/>
+
+<input type="hidden" name="subscriber.mobilenumber" value="820569065"/>
+
+
+
+
+
+</form>
+
+
+
+<input onclick="nithin()" name="submit" style="display: none;margin: auto;background: #52c600;color: white;font-weight: bold;padding: 10px;border: none;" value="Proceed to Pay" type="button">
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
 </html>
